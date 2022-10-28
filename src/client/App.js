99a3b865f3801b1d430b39baa127f895f9c16c8b
Deepanshu Gupta <312.gupta@gmail.com>
2022-10-28 11:44:54 -0400

    added bootstrap and basic App.js (client side) setup

--- src/client/App.js
+++ src/client/App.js
@@ -1,41 +1,28 @@
-import React, { Component, useState, useEffect } from 'react';
-import './app.css';
-import ReactImage from './react.png';
-
-// export default class App extends Component {
-//   state = { username: null };
-
-//   componentDidMount() {
-//     fetch('/api/getUsername')
-//       .then(res => res.json())
-//       .then(user => this.setState({ username: user.username }));
-//   }
-
-//   render() {
-//     const { username } = this.state;
-//     return (
-//       <div>
-//         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-//         <img src={ReactImage} alt="react" />
-//       </div>
-//     );
-//   }
-// }
+import React, { useState, useEffect } from "react";
+import "./app.css";
+import ReactImage from "./react.png";
 
 export default function App() {
   const [userName, setUserName] = useState(null);
 
   useEffect(() => {
-    console.log("Coming in Effect.. ")
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => setUserName(user.username));
-  }, [userName])
+    console.log("Coming in userName Effect.. ");
+    fetch("/api/getUsername")
+      .then((res) => res.json())
+      .then((user) => setUserName(user.username));
+
+  }, [userName]);
 
   return (
-    <div>
-      {userName ? <h1>{`Hello ${userName}`}</h1> : <h1>Loading.. please wait!</h1>}
-      <img src={ReactImage} alt="react" />
-    </div>
-  )
+    <>
+      <div>
+        {userName ? (
+          <h1>{`Hello ${userName}`}</h1>
+        ) : (
+          <h1>Loading.. please wait!</h1>
+        )}
+        <img src={ReactImage} alt="react" />
+      </div>
+    </>
+  );
 }
